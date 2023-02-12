5af2f0884f6e36999120f8fbbcd96d54b08e8e3d
Feyaaz <feyaazc94@gmail.com>
2023-02-12 12:47:59 +0000

    Add New SQL Statement

--- app/sql_config.py
+++ app/sql_config.py
@@ -1,4 +1,4 @@
-CREATE_ACCOUNTS_TABLE = "CREATE TABLE IF NOT EXISTS accounts (id SERIAL PRIMARY KEY, email TEXT, password TEXT);"
+CREATE_ACCOUNTS_TABLE = "CREATE TABLE IF NOT EXISTS accounts (id SERIAL PRIMARY KEY, email TEXT, password TEXT, created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
 
 CREATE_BOOKS_TABLE = (
     "CREATE TABLE IF NOT EXISTS books (id SERIAL PRIMARY KEY, title TEXT, isbn TEXT);"
@@ -6,7 +6,7 @@ CREATE_BOOKS_TABLE = (
 
 CREATE_PUBLISHERS_TABLE = "CREATE TABLE IF NOT EXISTS publishers (books_id INTEGER, author TEXT, publisher_name TEXT, publisher_date TEXT, FOREIGN KEY (books_id) REFERENCES books(id) ON DELETE CASCADE);"
 
-CREATE_S3_TABLE = "CREATE TABLE IF NOT EXISTS aws_s3 (id SERIAL PRIMARY KEY, bucket_name TEXT, file_name TEXT, file_path TEXT, account_id INTEGER, FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE);"
+CREATE_S3_TABLE = "CREATE TABLE IF NOT EXISTS aws_s3 (id SERIAL PRIMARY KEY, bucket_name TEXT, file_name TEXT, file_path TEXT, account_id INTEGER, uploaded_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE);"
 
 INSERT_ACCOUNTS = "INSERT INTO accounts (email, password) VALUES (%s, %s) RETURNING id;"
 
@@ -19,3 +19,5 @@ INSERT_S3_TABLE = "INSERT INTO aws_s3 (bucket_name, file_name, file_path, accoun
 CHECK_ACCOUNTS = "SELECT email FROM accounts WHERE email = %s;"
 
 SELECT_ACCOUNT_ID = "SELECT id FROM accounts WHERE email = %s;"
+
+SELECT_USERS_BOOKS = "SELECT file_name, uploaded_date FROM aws_s3 INNER JOIN accounts acc ON account_id = acc.id WHERE acc.id = %s;"
