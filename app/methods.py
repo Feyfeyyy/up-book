892cc17fd54d7a67d7e4a88c5409434e2e8c1087
Feyaaz <feyaazc94@gmail.com>
2023-02-13 00:02:07 +0000

    Update ISBN Validate Logic

--- app/methods.py
+++ app/methods.py
@@ -1,16 +1,41 @@
 def validate_isbn(isbn: str) -> bool:
     """
-    Validate ISBN-10 number. Returns True if valid, False otherwise.
+    Validate ISBN-10 and ISBN-13 numbers.
+
+    :param isbn: ISBN number
+    :return: True if valid, False otherwise
     """
     isbn = isbn.replace("-", "")
-    if len(isbn) != 10:
-        return False
-    sums = 0
-    for i in range(10):
-        if isbn[i].isdigit():
-            sums += int(isbn[i]) * (10 - i)
-        elif isbn[i] == "X" and i == 9:
-            sums += 10
-        else:
+    if len(isbn) == 10:
+        # ISBN-10 validation
+        if not isbn.isdigit():
             return False
-    return sums % 11 == 0
+        sums = 0
+        for i in range(9):
+            sums += (i + 1) * int(isbn[i])
+        check = sums % 11
+        if check == 10:
+            check = 'X'
+        else:
+            check = str(check)
+        if isbn[9] != check:
+            return False
+        return True
+    elif len(isbn) == 13:
+        # ISBN-13 validation
+        if not isbn.isdigit():
+            return False
+        sums = 0
+        for i in range(12):
+            if (i + 1) % 2 == 0:
+                sums += 3 * int(isbn[i])
+            else:
+                sums += int(isbn[i])
+        check = 10 - (sums % 10)
+        if check == 10:
+            check = 0
+        if int(isbn[12]) != check:
+            return False
+        return True
+    else:
+        return False
