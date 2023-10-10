7f90f85eba8319dd9f034774794526724e8a7964
Feyaaz <feyaazc94@gmail.com>
2023-10-10 17:15:17 +0100

    Refactor Validate Isbn Method

--- app/methods.py
+++ app/methods.py
@@ -2,40 +2,24 @@ def validate_isbn(isbn: str) -> bool:
     """
     Validate ISBN-10 and ISBN-13 numbers.
 
-    :param isbn: ISBN number
+    :param isbn: ISBN number to validate
     :return: True if valid, False otherwise
     """
-    isbn = isbn.replace("-", "")
+    # Remove hyphens and whitespace from the input ISBN
+    isbn = isbn.replace("-", "").replace(" ", "")
+
     if len(isbn) == 10:
         # ISBN-10 validation
-        if not isbn.isdigit():
+        if not isbn[:-1].isdigit() or (isbn[-1] != 'X' and not isbn[-1].isdigit()):
             return False
-        sums = 0
-        for i in range(9):
-            sums += (i + 1) * int(isbn[i])
-        check = sums % 11
-        if check == 10:
-            check = "X"
-        else:
-            check = str(check)
-        if isbn[9] != check:
-            return False
-        return True
+        check = sum((i + 1) * int(digit) for i, digit in enumerate(isbn[:-1])) % 11
+        check = 'X' if check == 10 else str(check)
+        return isbn[-1] == check
     elif len(isbn) == 13:
         # ISBN-13 validation
         if not isbn.isdigit():
             return False
-        sums = 0
-        for i in range(12):
-            if (i + 1) % 2 == 0:
-                sums += 3 * int(isbn[i])
-            else:
-                sums += int(isbn[i])
-        check = 10 - (sums % 10)
-        if check == 10:
-            check = 0
-        if int(isbn[12]) != check:
-            return False
-        return True
+        check = (10 - sum(int(digit) * (3 if i % 2 else 1) for i, digit in enumerate(isbn[:-1]))) % 10
+        return int(isbn[-1]) == check
     else:
         return False
