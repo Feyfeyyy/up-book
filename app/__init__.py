71a624e4b57ca611cd1de2d9e6e947118c3c0db0
Feyaaz <feyaazc94@gmail.com>
2024-04-29 16:16:28 +0100

    Update ENV & Config

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,11 @@ load_dotenv(".env")
 app = Flask(__name__)
 app.config.from_pyfile("environment.py")
 
-if app.config["ENV"] == "production":
+if app.config["ENV_KEY"] == "production":
     app.config.from_object("config.ProductionConfig")
-elif app.config["ENV"] == "testing":
+elif app.config["ENV_KEY"] == "testing":
     app.config.from_object("config.TestingConfig")
-elif app.config["ENV"] == "development":
+elif app.config["ENV_KEY"] == "development":
     app.config.from_object("config.DevelopmentConfig")
 
 from app import views
